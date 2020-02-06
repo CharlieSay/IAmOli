6f094b81f15f2f98c35192908fd8c1012b7f3ea3
CharlieSay <charlie.w.say@live.co.uk>
2020-02-06 07:03:19 +0000

    Header spacing + SVG's

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,17 @@
 import React, { Fragment } from "react";
-import './header.scss'
+import "./header.scss";
 
 export const Header = () => {
   return (
     <Fragment>
       <div className="header_parent o-page-align">
-        <h1 style={{ alignContent: "center " }}> I AM OLI  </h1>
+        <div className="navigation App-medium">
+          <span> STORE </span>
+          <span> ABOUT </span>
+          <span> PORTFOLIO </span>
+        </div>
+        <h1 style={{ alignContent: "center" }}> I AM OLI </h1>
+        <h1 style={{ alignContent: "center" }}> TFIL </h1>
       </div>
     </Fragment>
   );
