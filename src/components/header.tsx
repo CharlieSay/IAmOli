46271930c3e90597345772baeb069cb103b36d19
CharlieSay <charlie.w.say@live.co.uk>
2020-02-12 18:27:42 +0000

    Changed header to accomodate mobile and dekstop better

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,42 +2,45 @@ import React, { Fragment } from "react";
 import { useMediaQuery } from "react-responsive";
 import { Navigation } from "./navigation";
 import { SocialHeader } from "./social";
+import iconNav from "../assets/svgs/icon-nav.svg";
+
 import "./header.scss";
 
 export const Header = () => {
-  const isMobile = useMediaQuery({ query: "(max-width: 460px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 459px)" });
   const isDesktop = useMediaQuery({ query: "(min-device-width: 460px)" });
-  const isLandscape = useMediaQuery({ query: '(orientation: landscape)' })
 
   return (
     <Fragment>
       <div className="header_parent o-page-align">
         {isDesktop && (
-          <div className="desktop">
+          <Fragment>
             <div className="child-container">
               <Navigation />
             </div>
             <div className="logo-container">
-              <a href="/" className="logo-child">
+              <a href="/" className="logo-child-desktop">
                 I AM OLI
               </a>
             </div>
             <div className="child-container">
               <SocialHeader />
             </div>
-          </div>
+          </Fragment>
         )}
         {isMobile && (
-          <div className="mobile-header">
-            <div className="child-container">
-                <img src={'src/assets/svgs/icon-nav.svg'} />
+          <Fragment>
+            <div className="mobile-header">
+              <div className="mobile-header__nav_container">
+                <img src={iconNav} />
+              </div>
+              <div className="logo-container">
+                <a href="/">
+                  <span className="logo-child-mobile">I AM OLI</span>
+                </a>
+              </div>
             </div>
-            <div className="logo-container">
-              <a href="/" className="logo-child">
-                MOBILE HEADER
-              </a>
-            </div>
-          </div>
+          </Fragment>
         )}
       </div>
     </Fragment>
