852e2874e54aec665c70d7bb023c4c05fc1563a4
CharlieSay <charlie.w.say@live.co.uk>
2020-02-12 20:41:36 +0000

    various fixes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,31 @@
 import React from "react";
-import './footer.scss'
+import "./footer.scss";
+import { useMediaQuery } from "react-responsive";
 
 export const Footer = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 459px)" });
+  const isDesktop = useMediaQuery({ query: "(min-device-width: 460px)" });
+
   return (
-    <div className="parent o-page-align">
-      <div className="child">
-        <h4>Contact me</h4>
-      </div>
-      <div className="child">
-        <h3>I AM OLI</h3>
-      </div>
-      <div className="child">
-        <h4>SOCIAL MEDIA</h4>
-      </div>
+    <div className="app-medium o-page-align">
+      {isDesktop && (
+        <div className="footer-parent">
+          <div className="child">
+            <span>COPYRIGHT © IAMOLI</span>
+          </div>
+          <div className="child">
+            <span>CONTACT ME</span>
+          </div>
+          <div className="child">
+            <span>WEBSITE BY CHARLIE SAY</span>
+          </div>
+        </div>
+      )}
+      {isMobile && (
+        <div className="child__mobile">
+          <span>COPYRIGHT © IAMOLI</span>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,22 +1,47 @@
 import React, { Fragment } from "react";
+import heroImage from "../assets/img/hero.png";
+import { useMediaQuery } from "react-responsive";
+
+import "../components/page1.scss";
 
 const Page1 = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 459px)" });
+  const isDesktop = useMediaQuery({ query: "(min-device-width: 460px)" });  
+
   return (
     <Fragment>
       <div
-        className="App"
+        className="App  "
         style={{ display: "nowrap", justifyContent: "center" }}
       >
-        <h1> Home Page </h1>
-        <div>
-          <a
-            className="App-link"
-            href="https://instagram.com/charliesay"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            INSTAGRAM
-          </a>
+        <div className="page-container o-page-spacer">
+          <div>
+            <a href="https://www.youtube.com/watch?v=t6S0U0dxcBY">
+              {isDesktop && (
+                <img
+                  className="heroImage"
+                  style={{ flex: "nowrap" }}
+                  src={heroImage}
+                />
+              )}
+              {isMobile && (
+                 <img
+                 className="heroImage-mobile"
+                 style={{ flex: "nowrap" }}
+                 src={heroImage}
+               />
+              )}
+            </a>
+          </div>
+          <div className="o-page-align section-one">
+            <a
+              href="https://instagram.com/charliesay"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              INSTAGRAM
+            </a>
+          </div>
         </div>
       </div>
     </Fragment>
