49b8126caa13177750b67be1caff15882c47d089
Charlie Say <charlie.say@autotrader.co.uk>
2020-02-17 13:50:06 +0000

    Header in better place

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,16 +15,25 @@ export const Header = () => {
       <div className="header__parent app">
         {isDesktop && (
           <div className="header__row">
-            <section>
-              <Navigation />
+            <section className="app-medium">
+              <ul>
+                <a href="/page2">STORE</a>
+                <a href="/about">ABOUT</a>
+                <a href="/portfolio">PORTFOLIO</a>
+              </ul>
             </section>
             <section>
               <a href="/">
                 <span>I AM OLI</span>
               </a>
             </section>
-            <section>
-              <SocialHeader />
+            <section className="app-medium">
+              <ul>
+                <a href="//www.twitter.com"> T </a>
+                <a href="//www.facebook.com"> F </a>
+                <a href="//www.instagram.com"> I </a>
+                <a href="//www.vimeo.com"> V </a>
+              </ul>
             </section>
           </div>
         )}
