fa958edd5f195bb26b286abc0fe248892e7dfd6b
Charlie Say <charlie.say@autotrader.co.uk>
2020-02-17 18:12:59 +0000

    HEADERS LOOK GOOD FINALLY

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,36 +6,56 @@ import "../scss/components/header.scss";
 
 export const Header = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 459px)" });
-  const isDesktop = useMediaQuery({ query: "(min-device-width: 460px)" });
+  const isMedDesktopLow = useMediaQuery({ query: "(min-device-width: 460px)" });
+  const isMedDesktopHigh = useMediaQuery({ query: "(max-width: 744px)" });
+  const isWideDesktop = useMediaQuery({ query: "(min-device-width: 745px)" });
 
   return (
-    <header>
-      <div className="header__parent app">
-        {isDesktop && (
-          <div className="header__row">
-            <section className="app-medium">
-              <ul>
-                <a href="/page2">STORE</a>
-                <a href="/about">ABOUT</a>
-                <a href="/portfolio">PORTFOLIO</a>
-              </ul>
-            </section>
-            <section>
+    <header className="header__parent app">
+      {isWideDesktop && (
+        <div className="header__row">
+          <section>
+            <ul className="app-medium header__item header__item__navigation">
+              <a href="/page2">STORE</a>
+              <a href="/about">ABOUT</a>
+              <a href="/portfolio">PORTFOLIO</a>
+            </ul>
+          </section>
+          <section>
+            <ul className="header__item header__item__name">
               <a href="/">
                 <span>I AM OLI</span>
               </a>
-            </section>
-            <section className="app-medium">
-              <ul>
-                <a href="//www.twitter.com"> T </a>
-                <a href="//www.facebook.com"> F </a>
-                <a href="//www.instagram.com"> I </a>
-                <a href="//www.vimeo.com"> V </a>
-              </ul>
-            </section>
-          </div>
-        )}
-      </div>
+            </ul>
+          </section>
+          <section>
+            <ul className="app-medium header__item header__item__socials">
+              <a href="//www.twitter.com"> T </a>
+              <a href="//www.facebook.com"> F </a>
+              <a href="//www.instagram.com"> I </a>
+              <a href="//www.vimeo.com"> V </a>
+            </ul>
+          </section>
+        </div>
+      )} 
+      {(isMedDesktopLow && isMedDesktopHigh) && (
+        <div className="header__row">
+          <section>
+            <ul className="app-medium header__item header__item__navigation">
+              <a href="/page2">STORE</a>
+              <a href="/about">ABOUT</a>
+              <a href="/portfolio">PORTFOLIO</a>
+            </ul>
+          </section>
+          <section>
+            <ul className="header__item header__item__name">
+              <a href="/">
+                <span>I AM OLI</span>
+              </a>
+            </ul>
+          </section>
+        </div>
+      )}
       {isMobile && (
         <div className="mobile-header">
           <div className="hamburger_nav">
