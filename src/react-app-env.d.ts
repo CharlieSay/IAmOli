49b452855489805f478c2e83448a40b27897b659
Charlie Say <charlie.say@autotrader.co.uk>
2020-02-22 20:45:57 +0000

    Fix compile error.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,5 @@ declare module 'react-router-dom';
 declare module 'react-responsive';
 declare module 'react-fade-in';
 declare module 'react-scroll-horizontal';
+declare module 'react-router';
+declare module 'react-dropdown';
